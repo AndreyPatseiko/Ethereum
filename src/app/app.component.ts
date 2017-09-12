058651fa108e87cb3c4a75c6ef628197e7bd8d28
Andrey Patseiko <a.patseika@omertex.com>
2017-09-12 23:59:04 +0300

    save json file in pc | problem for loading json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EthereumService} from  './ethereum.service';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import * as FileSaver from 'file-saver';
 import Web3 from 'web3';
 // import {Accounts} from 'web3-eth-accounts';
 
@@ -15,7 +17,7 @@ export class AppComponent implements OnInit {
   connection = false;
   web3: Web3;
 
-  constructor(private ethService: EthereumService) {
+  constructor(private ethService: EthereumService, private _http: Http) {
     this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
 
@@ -48,5 +50,24 @@ export class AppComponent implements OnInit {
   }
 
   cb(data?) {    
+  }
+
+  // ===================================================
+  // ===============  Save and load File  ==============
+
+  saveFile() {
+    const fileContent = {test: 'test message', testData: 'test data for blob file'};
+    const blob = new Blob([JSON.stringify(fileContent, null, 2)], {type: 'application/json'});
+    FileSaver.saveAs(blob, 'test-export.json');
+  }
+
+  getFileFromPc() {
+    console.log('upload file')
+    this._http.get('file://C:/Users/Olya/Downloads/test-export.json')
+    // this._http.get('/assets/test-export.json')
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {EthereumService} from  './ethereum.service';
 import {AppComponent} from './app.component';
-
+import {HttpModule} from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [EthereumService],
   bootstrap: [AppComponent]
